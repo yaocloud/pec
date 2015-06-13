99c0cf52ee88ade6fdf129c2fb771ceaab8af33a
kazuhiko yamahsita <pyama@pepabo.com>
2015-06-13 15:29:18 +0900

    delete unused method

--- lib/pec/compute/security_group.rb
+++ lib/pec/compute/security_group.rb
@@ -2,11 +2,6 @@ module Pec
   class Compute
     class Security_Group
       include Query
-      def add_security_group(server_id, security_groups)
-        security_groups.each do |sg_name|
-          response = Fog::Compute[:openstack].add_security_group(server_id, sg_name)
-        end if security_groups
-      end
     end
   end
 end
