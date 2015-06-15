6446017fb7bdb98df22ebf73ee9cc51170442d34
kazuhiko yamahsita <pyama@pepabo.com>
2015-06-16 01:13:59 +0900

    append delete response

--- lib/pec/compute/server.rb
+++ lib/pec/compute/server.rb
@@ -4,7 +4,6 @@ module Pec
       extend Query
       class << self
         def create(name, image_ref, flavor_ref, options)
-
           response = Pec::Resource.get.create_server(name, image_ref, flavor_ref, options)
           raise(Pec::Errors::Host, "server_name:#{name} response err status:#{response[:status]}") unless response[:status] == 202
           puts "success create for server_name:#{name.to_s}".blue
@@ -23,6 +22,7 @@ module Pec
 
           raise(Pec::Errors::Host, "server_name:#{name} response err status:#{response[:status]}") unless response[:status] == 204
           puts "server_name:#{server_name} is deleted!".green
+          true
         end
       end
     end
