4344169e1382b7d5e51e1f63b10dede9c4c10fd6
pyama86 <pyama@pepabo.com>
2015-08-24 00:00:52 +0900

    delete duplicate

--- lib/pec.rb
+++ lib/pec.rb
@@ -45,12 +45,13 @@ module Pec
 end
 
 class ::Hash
+
   def deep_merge(second)
-      merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : Array === v1 && Array === v2 ? v1 | v2 : [:undefined, nil, :nil].include?(v2) ? v1 : v2 }
-      self.merge(second.to_h, &merger)
+    merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : Array === v1 && Array === v2 ? v1 | v2 : [:undefined, nil, :nil].include?(v2) ? v1 : v2 }
+    self.merge(second.to_h, &merger)
   end
+
   def deep_merge!(second)
-      merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : Array === v1 && Array === v2 ? v1 | v2 : [:undefined, nil, :nil].include?(v2) ? v1 : v2 }
-      self.merge!(second.to_h, &merger)
+    self.merge!(deep_merge(second))
   end
 end
