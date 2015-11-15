9c0cdd1bb62543e89198401d2e22250427044854
pyama86 <pyama@pepabo.com>
2015-11-15 14:30:42 +0900

    added halt command

--- lib/pec/command.rb
+++ lib/pec/command.rb
@@ -3,6 +3,7 @@ module Pec
     autoload :Base,    "pec/command/base"
     autoload :Up,      "pec/command/up"
     autoload :Destroy, "pec/command/destroy"
+    autoload :Halt,    "pec/command/halt"
     autoload :Status,  "pec/command/status"
     autoload :Config,  "pec/command/config"
     autoload :Init,    "pec/command/init"
