4998644fc95a82dd62da230fc31d28ad89588435
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-05-30 21:29:37 +0900

    fix specs

--- spec/lib/handler/user_data_spec.rb
+++ spec/lib/handler/user_data_spec.rb
@@ -51,13 +51,13 @@ describe Pec::Handler::UserData do
       expect(subject).to eq(
         [
           {
-            "content" => "NAME=eth0\nDEVICE=eth0\nTYPE=Ethernet\nONBOOT=yes\nHWADDR=00:00:00:00:00:00\nNETMASK=255.255.255.0\nIPADDR=10.10.10.10\nBOOTPROTO=static\nDNS-NAMESERVERS=8.8.8.8\nGATEWAY=1.1.1.1",
+            "content" => "NAME=eth0\nDEVICE=eth0\nTYPE=Ethernet\nONBOOT=yes\nHWADDR=00:00:00:00:00:00\nNETMASK=255.255.255.0\nIPADDR=10.10.10.10\nBOOTPROTO=static\nDNS-NAMESERVERS=8.8.8.8\nGATEWAY=1.1.1.1\n",
             "owner" => "root:root",
             "path" => "/etc/sysconfig/network-scripts/ifcfg-eth0",
             "permissions" => "0644"
           },
           {
-            "content" => "NAME=eth1\nDEVICE=eth1\nTYPE=Ethernet\nONBOOT=yes\nHWADDR=00:00:00:00:00:00\nNETMASK=255.255.255.0\nIPADDR=20.20.20.20\nBOOTPROTO=static\nDNS-NAMESERVERS=8.8.8.8\nGATEWAY=2.2.2.2",
+            "content" => "NAME=eth1\nDEVICE=eth1\nTYPE=Ethernet\nONBOOT=yes\nHWADDR=00:00:00:00:00:00\nNETMASK=255.255.255.0\nIPADDR=20.20.20.20\nBOOTPROTO=static\nDNS-NAMESERVERS=8.8.8.8\nGATEWAY=2.2.2.2\n",
             "owner" => "root:root",
             "path" => "/etc/sysconfig/network-scripts/ifcfg-eth1",
             "permissions" => "0644"
